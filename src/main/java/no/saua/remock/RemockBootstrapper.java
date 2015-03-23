0a2adad5758b58d442764623d637d2e1bacbdb9d
Knut Saua Mathiesen <git@canutes.net>
2015-03-23 09:29:24 +0100

    Fix issue with mocks not getting reset

--- src/main/java/no/saua/remock/RemockBootstrapper.java
+++ src/main/java/no/saua/remock/RemockBootstrapper.java
@@ -3,11 +3,16 @@ package no.saua.remock;
 import no.saua.remock.internal.RemockBeanFactory;
 import no.saua.remock.internal.RemockContextClassLoader;
 import no.saua.remock.internal.RemockMergedContextConfiguration;
+import no.saua.remock.internal.RemockTestExecutionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextLoader;
 import org.springframework.test.context.MergedContextConfiguration;
 import org.springframework.test.context.support.AbstractTestContextBootstrapper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Bootstrapper for Remock. Sets up the context loader {@link RemockContextClassLoader}.
@@ -25,5 +30,13 @@ public class RemockBootstrapper extends AbstractTestContextBootstrapper {
     protected MergedContextConfiguration processMergedContextConfiguration(MergedContextConfiguration mergedConfig) {
         log.debug("Using Remock's MergecContextConfiguration");
         return new RemockMergedContextConfiguration(mergedConfig);
+    }
+
+    @Override
+    protected List<String> getDefaultTestExecutionListenerClassNames() {
+        List<String> classes = new ArrayList<>();
+        classes.addAll(super.getDefaultTestExecutionListenerClassNames());
+        classes.add(RemockTestExecutionListener.class.getName());
+        return Collections.unmodifiableList(classes);
     }
 }
